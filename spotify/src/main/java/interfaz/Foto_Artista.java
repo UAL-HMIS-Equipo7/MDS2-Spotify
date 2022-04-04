3f1233e9624781b1cc8b4232430aeeb1d7bc3f93
Manuel David Montesinos Navarrete <mmn133@inlumine.ual.es>
2022-04-04 14:50:21 +0200

    Proyecto Compilando.

--- spotify/src/main/java/interfaz/Foto_Artista.java
+++ spotify/src/main/java/interfaz/Foto_Artista.java
@@ -8,7 +8,7 @@ public class Foto_Artista extends VistaFoto_artista {
 	public Ver_perfil_ajeno_de_artista _ver_perfil_ajeno_de_artista;
 	
 	public Foto_Artista() {
-		_ver_perfil_ajeno_de_artista = new Ver_perfil_ajeno_de_artista();
+		//_ver_perfil_ajeno_de_artista = new Ver_perfil_ajeno_de_artista();
 		
 		this.getNickB().setVisible(false);
 	}
