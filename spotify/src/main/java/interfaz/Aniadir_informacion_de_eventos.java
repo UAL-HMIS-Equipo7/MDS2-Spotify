086ecfa530b79815f8f72f5082677cf162f34dc1
MATÍAS GARCÍA GIMENO <mgg709@inlumine.ual.es>
2022-06-08 12:45:31 +0200

    Eventos funcionando

--- spotify/src/main/java/interfaz/Aniadir_informacion_de_eventos.java
+++ spotify/src/main/java/interfaz/Aniadir_informacion_de_eventos.java
@@ -81,9 +81,8 @@ public class Aniadir_informacion_de_eventos extends VistaAniadir_informacion_de_
 	}
 
 	public boolean Validar_informacion_evento() {
-		//VALIDAR
 		boolean correcto = true;
-		if(this.getFechaHoraTF().getValue().isBlank() || this.getVaadinTextArea().getValue().isBlank() || rutaFoto == null || rutaFoto.isBlank()  || this.getPrecioTF().getValue().isBlank()) {
+		if(this.getFechaHoraTF().getValue().isBlank() || this.getVaadinTextArea().getValue().isBlank()  || _img.getSrc() == null || _img.getSrc().isBlank()  || this.getPrecioTF().getValue().isBlank()) {
 			correcto = false;
 		}
 		return correcto;
